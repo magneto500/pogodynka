1730483a5bd581d9aee61a406e36f1d673ae3fe8
Mateusz <gawinmateusz@gmail.com>
2022-01-19 22:12:43 +0100

    Implementation of new feature -> get all locations.

--- src/main/java/com/sda/weather/location/LocationRepositoryImpl.java
+++ src/main/java/com/sda/weather/location/LocationRepositoryImpl.java
@@ -5,11 +5,14 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 public class LocationRepositoryImpl implements LocationRepository {
 
     private final SessionFactory sessionFactory;
 
+    @Override
     public Location save(Location location) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
@@ -18,4 +21,16 @@ public class LocationRepositoryImpl implements LocationRepository {
         session.close();
         return location;
     }
+
+    @Override
+    public List<Location> findAll() {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Location> locations = session.createQuery("FROM Location", Location.class).getResultList();
+        transaction.commit();
+        session.close();
+        return locations;
+    }
+
+
 }
--- src/test/java/com/sda/weather/location/LocationRepositoryMock.java
+++ src/test/java/com/sda/weather/location/LocationRepositoryMock.java
@@ -1,9 +1,18 @@
 package com.sda.weather.location;
 
+import java.util.Collections;
+import java.util.List;
+
 public class LocationRepositoryMock implements LocationRepository {
+
     @Override
     public Location save(Location location) {
         location.setId(1L);
         return location;
+    }
+
+    @Override
+    public List<Location> findAll() {
+        return Collections.emptyList();
     }
 }
