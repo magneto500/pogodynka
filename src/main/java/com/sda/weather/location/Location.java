125b525d25138ebfc904e42b5f8b3b93ad7ca193
magneto500 <tomasz.piotr.grabowski@gmail.com>
2022-02-01 09:38:13 +0100

    - nie mogę zobaczyć graficznej reprezentacji tabeli na h2 - gdy zmienię username / pass już nie zaloguje się do  h2

--- src/main/java/com/sda/weather/location/Location.java
+++ src/main/java/com/sda/weather/location/Location.java
@@ -2,17 +2,19 @@ package com.sda.weather.location;
 
 import lombok.Data;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 
 @Data
 @Entity
+@Table(name="tabela")
 public class Location {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String cityName;
     private double latitude;
--- src/main/java/com/sda/weather/location/LocationController.java
+++ src/main/java/com/sda/weather/location/LocationController.java
@@ -1,6 +1,4 @@
 package com.sda.weather.location;
-
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
